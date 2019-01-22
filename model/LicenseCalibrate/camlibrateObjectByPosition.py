ca989d173cd779d00508e16bd3c50e1804360114
xiaojing <jing.xiao@nttdata.com>
2019-01-22 18:58:40 +0800

    ---

--- model/LicenseCalibrate/camlibrateObjectByPosition.py
+++ model/LicenseCalibrate/camlibrateObjectByPosition.py
@@ -6,7 +6,10 @@ date: 2019/1/2
 Author: xiaojing
 """
 import os
+from PIL import Image
+
 import cv2
+import pytesseract
 
 
 def binarization(img):
@@ -23,12 +26,13 @@ def binarization(img):
     # 膨胀
     dilate = cv2.dilate(blurred, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
     # 腐蚀
-    # erode = cv2.erode(dilate, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
+    erode = cv2.erode(dilate, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
     # 膨胀
-    # dilate1 = cv2.dilate(dilate, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
+    dilate1 = cv2.dilate(erode, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
 
     # 自适应阈值化能够根据图像不同区域亮度分布，改变阈值
-    thresh = cv2.adaptiveThreshold(dilate, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 10)
+    thresh = cv2.adaptiveThreshold(dilate1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 10)
+    # thresh = cv2.threshold(dilate, 125, 250, cv2.THRESH_BINARY_INV, 0)
     cv2.imshow("thresh", thresh)
 
     return thresh
@@ -64,6 +68,28 @@ def openCamera():
         # 框选文字“二种”
         cv2.rectangle(frame_copy, (60, 363), (90, 390), (0, 255, 0), 1)
 
+        # detect
+        info1 = pytesseract.image_to_string(Image.fromarray(image1))
+        info2 = pytesseract.image_to_string(Image.fromarray(image2))
+        info3 = pytesseract.image_to_string(Image.fromarray(image3))
+        info4 = pytesseract.image_to_string(Image.fromarray(image4))
+        info5 = pytesseract.image_to_string(Image.fromarray(image5))
+
+        # 框选氏名
+        cv2.putText(frame_copy, info1, (60, 77), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
+
+        # 框选名字
+        cv2.putText(frame_copy, info2, (106, 77), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
+
+        # 框选住址
+        cv2.putText(frame_copy, info3, (106, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
+
+        # 框选文字“生”
+        cv2.putText(frame_copy, info4, (566, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
+
+        # 框选文字“二种”
+        cv2.putText(frame_copy, info5, (67, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
+
         cv2.imshow("frame_copy", frame_copy)
 
         key = cv2.waitKey(1)
@@ -71,58 +97,16 @@ def openCamera():
             break
         elif key == ord('s'):
 
-            h = frame.shape[0]
-            w = frame.shape[1]
-
-            for row in range(h):  # 遍历高
-                for col in range(w):  # 遍历宽
-                    pv = frame[row, col]
-                    frame[row, col] = 255 - pv
-
-            cv2.imwrite("thresh.jpg", frame)
-
-            cv2.destroyAllWindows()
-
-            cv2.imwrite("image1.jpg", image1)
-            cv2.imwrite("image2.jpg", image2)
-            cv2.imwrite("image3.jpg", image3)
-            cv2.imwrite("image4.jpg", image4)
-            cv2.imwrite("image5.jpg", image5)
-            # -psd 7 表示告诉tesseract code.jpg图片是一行文本  这个参数可以减少识别错误率.默认为 3
-
-            os.popen("tesseract image1.jpg image1 -l jpn -psd 7")
-            info1 = read_file("image1.txt")
-
-            os.popen("tesseract image2.jpg image2 -l jpn -psd 7")
-            info2 = read_file("image2.txt")
-
-            os.popen("tesseract image3.jpg image3 -l jpn -psd 7")
-            info3 = read_file("image3.txt")
-
-            os.popen("tesseract image4.jpg image4 -l jpn -psd 7")
-            info4 = read_file("image4.txt")
-
-            os.popen("tesseract image5.jpg image5 -l jpn -psd 7")
-            info5 = read_file("image5.txt")
-
-            # 框选氏名
-            cv2.putText(frame_copy, info1, (60, 77), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-            # 框选名字
-            cv2.putText(frame_copy, info2, (106, 77), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-            # 框选住址
-            cv2.putText(frame_copy, info3, (106, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-            # 框选文字“生”
-            cv2.putText(frame_copy, info4, (566, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-            # 框选文字“二种”
-            cv2.putText(frame_copy, info5, (67, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-            cv2.imshow("frame_copy", frame_copy)
-
-            cv2.waitKey(0)
+            # h = frame.shape[0]
+            # w = frame.shape[1]
+            #
+            # for row in range(h):  # 遍历高
+            #     for col in range(w):  # 遍历宽
+            #         pv = frame[row, col]
+            #         frame[row, col] = 255 - pv
+            #
+            # cv2.imwrite("thresh.jpg", frame)
+            # cv2.destroyAllWindows()
             break
 
 
@@ -139,9 +123,3 @@ def read_file(file):
 
 if __name__ == '__main__':
     openCamera()
-    # os.popen("tesseract image1.jpg image1 -l jpn")
-    # os.popen("tesseract image2.jpg image2 -l jpn")
-    # os.popen("tesseract image3.jpg image3 -l jpn")
-    # os.popen("tesseract image4.jpg image4 -l jpn")
-    # os.popen("tesseract picture/hello.tiff image5 -l jpn")
-    # read_file("image3.txt")
--- model/LicenseCalibrate/netCamera.py
+++ model/LicenseCalibrate/netCamera.py
@@ -1,9 +1,19 @@
 import cv2
+from PIL import Image
+
+import pytesseract
+
 
 cap = cv2.VideoCapture(0)
 i = 0
 while (1):
     ret, frame = cap.read()
+
+    # 框选氏名
+    cv2.rectangle(frame.copy(), (60, 77), (106, 105), (0, 255, 0), 1)
+
+    pytesseract.image_to_string(Image.open("1234.png", 'r'), lang="chi")
+
     k = cv2.waitKey(1)
     if k == 27:
         break
